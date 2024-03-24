d70445e6813c4349c7752a441129ec36bfefa4f7
MarinaMaier <157530878+MarinaMaier@users.noreply.github.com>
2024-03-24 19:21:53 -0400

    add header and logo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
+
 const localizer = momentLocalizer(moment);
 
 export function Home() {
