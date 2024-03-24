6b0b9c3c2d290c108024141bf13a1b15a0d9e480
MarinaMaier <157530878+MarinaMaier@users.noreply.github.com>
2024-03-24 19:07:40 -0400

    add components, assets, home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+export function Home() {
+  return (
+    <div className="Home">
+      <Calendar
+        localizer={localizer}
+        events={[]}
+        startAccessor="start"
+        endAccessor="end"
+        views={['week', 'month']}
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+}
