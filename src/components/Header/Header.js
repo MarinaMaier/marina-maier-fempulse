f0f0054584ae7c2daf07e22b360c0ae6aa6c443c
MarinaMaier <157530878+MarinaMaier@users.noreply.github.com>
2024-04-01 12:02:34 -0400

    add backend integration, styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import logo from "../../assets/logo-fempulse.png";
+import logoutIcon from "../../assets/door-closed-fill.svg";
 import "./Header.scss";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 export function Header() {
-    return (
-        <div className="header">
-            <img className="header__logo" src={logo} alt="fempulse" />
+  const logout = () => {};
+  return (
+    <div className="header">
+      <img className="header__logo" src={logo} alt="fempulse" />
+      <OverlayTrigger
+        placement="bottom"
+        overlay={
+          <Tooltip className="tooltip-custom" id="tooltip">
+            Logout
+          </Tooltip>
+        }
+      >
+        <div className="header__logout-icon" onClick={() => logout()}>
+          <img src={logoutIcon} alt="mood" />
         </div>
-    )
+      </OverlayTrigger>
+    </div>
+  );
 }
